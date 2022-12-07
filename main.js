51295c309272f4f12c2a22b53614e4d2151b8aa8
Bruno Espindola de Brito <77520947+Bruno-Espindola-Brito@users.noreply.github.com>
2022-12-07 19:26:47 -0300

    Add files via upload
    Projeto finalizado

---
+++ main.js
@@ -0,0 +1,43 @@
+function tocaSom (seletorElemento) {
+
+    const elemento = document.querySelector(seletorElemento);
+
+    if (elemento != null && elemento.localName === 'audio') {
+      elemento.play();
+    }
+
+    else {
+      alert('Elemento não encontrado');
+    }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`;//Template string
+
+  tecla.onclick = function () {
+      tocaSom(idAudio);
+    }
+  
+  tecla.onkeydown = function (evento) {
+
+    if (evento.code === 'Space' || evento.code === 'Enter')
+      tecla.classList.add('ativa');
+  }
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove('ativa');
+  }
+    
+}
+  
+  
+
+   
+
+ 
